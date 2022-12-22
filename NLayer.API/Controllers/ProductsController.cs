bea444715c59eabfe25764174b1ae5a93aab9d83
enesyld <enesbote.44@gmail.com>
2022-12-22 16:52:03 +0300

    Add project files.

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CostumResponseDto<List<ProductDto>>.Success(200, productsDtos));
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CostumResponseDto<ProductDto>.Success(200, productsDto));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductUpdateDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CostumResponseDto<ProductDto>.Success(201, productsDto));
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CostumResponseDto<NoContentDto>.Success(204));
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+
+            return CreateActionResult(CostumResponseDto<NoContentDto>.Success(200));
+
+        }
+    }
+}
---
+++ NLayer.Core/DTOs/CostumResponseDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CostumResponseDto<T>
+    {
+        public T Data { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public List<String> Errors { get; set; }
+
+        public static CostumResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CostumResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+        public static CostumResponseDto<T> Success(int statusCode)
+        {
+            return new CostumResponseDto<T> {StatusCode = statusCode};
+
+        }
+        public static CostumResponseDto<T> Fail(int statusCode,List<string> errors)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode, Errors = errors };
+
+        }
+        public static CostumResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode, Errors =new List<string> { error } };
+
+        }
+    }
+}
